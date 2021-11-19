44d1c6731204abe076f13494df4bb70a81736bae
KC1919 <kunal.chandra1900@gmail.com>
2021-11-19 22:38:19 +0530

    search api completed

--- routes/getData.js
+++ routes/getData.js
@@ -1,47 +1,76 @@
-const express=require("express");
-const Data=require("../models/Data");
+const express = require("express");
+const Data = require("../models/Data");
 
-const router=express.Router();
+const router = express.Router();
 
-let skip=0, limit=15;
+let skip = 0,
+    limit = 15;
 
-router.get("/",async (req,res)=>{
+router.get("/", async (req, res) => {
 
-    const count=await Data.count();
-    // console.log(n);
+    const count = await Data.count();
+
     try {
-        const result= await Data.find({},{_id:0}).sort({
+        //fetching the documents with a limit of 15 documents per page
+        const result = await Data.find({}, {
+            _id: 0
+        }).sort({
             publishedAt: -1
         }).skip(skip).limit(limit); //applied pagination
 
-        if(result && skip<=count){
-            skip+=limit;
-            return res.status(200).json({message:"Documents fetched", result:result});
-        }
-        else{
-            return res.status(200).json({message:"Nothing found!", result:[]});
+        //if documents found in the databse
+        if (result && skip <= count) {
+            skip += limit;
+            return res.status(200).json({ //return the fethed docments
+                message: "Documents fetched",
+                result: result
+            });
+        } else {
+            return res.status(200).json({
+                message: "Nothing found!",
+                result: []
+            });
         }
     } catch (error) {
-        return console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error"
+        });
     }
 })
-                                                 
-router.post("/getData",async(req,res)=>{
-    try {
-        let term=req.body.term; //extracting the user search term
-        console.log(term);
-        //using the regex matching to search for all the documents in the database and fetch them
-        const result=await Data.find( {$or:[{ title:new RegExp(term,'i')},{ description:new RegExp(term,'i')}]} );
 
-        if(result){
-            return res.status(200).json({message:"Documents Fetched", result:result});
-        }else{
+router.post("/getData", async (req, res) => {
+    try {
+        let term = req.body.term; //extracting the user search term
+
+        //using the regex matching to search for all the documents in the database and fetch them
+        const result = await Data.find({
+            $or: [{
+                title: new RegExp(term, 'i') //matching term in title
+            }, {
+                description: new RegExp(term, 'i') //matching term in description
+            }]
+        });
+
+        //if document found
+        if (result) {
+            return res.status(200).json({ //return the results to the client
+                message: "Documents Fetched",
+                result: result
+            });
+        } else {
             console.log("No result found");
-            return res.status(200).json({message:"Nothing found!", result:[]});
+            return res.status(200).json({
+                message: "Nothing found!",
+                result: []
+            });
         }
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            eror: error
+        });
     }
 })
 
-module.exports=router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-const express = require("express");  //importing express package
-const app = express();       //initializing the express method
+const express = require("express"); //importing express package
+const app = express(); //initializing the express method
 require("dotenv").config();
+
 
 //Configuring express to parse json or urlencoded data from client side
 app.use(express.urlencoded({
     extended: false
 }))
+
+
+
 app.use(express.json());
 // app.set('view engine', 'ejs');
 
 
-const router=require("./routes/getData");
-app.use("/",router);
+//making a router to get all documents and to get documents by searched term
+const router = require("./routes/getData");
+app.use("/", router);
 
-const connectDB=require("./db");
-const fetch=require("./routes/fetchData");
+
+const connectDB = require("./db");
+const fetch = require("./routes/fetchData");
+
+
 
 //Running the server
 app.listen(process.env.PORT || 3000, async (req, res) => {
     console.log("Sever started");
     await connectDB();
-    // setInterval(fetch,10000);
+    setInterval(fetch, 10000); //fetching the documents every 10 seconds from the Youtube api
 });
