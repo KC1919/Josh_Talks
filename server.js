0cd04b1f8981b2fed0dd7259c8f39e30268369e1
KC1919 <kunal.chandra1900@gmail.com>
2021-11-19 12:35:33 +0530

    server set up

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");  //importing express package
+const app = express();       //initializing the express method
+require("dotenv").config();
+
+//Configuring express to parse json or urlencoded data from client side
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+
+
+//Running the server
+app.listen(process.env.PORT || 3000, (req, res) => {
+    console.log("Sever started");
+})
